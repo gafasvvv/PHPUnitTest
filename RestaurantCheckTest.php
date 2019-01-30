a08c54f93a836a846f518331707e46fc246435fa
gafasvvv <ry0suke.programming@gmail.com>
2019-01-30 10:23:34 +0900

    first phpunittest

---
+++ RestaurantCheckTest.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'restaurant-check.php';
+
+class RestaurantCheckTest extends \PHPUnit\Framework\TestCase{
+
+    public function testWithTaxAndTip(){
+        $meal = 100;
+        $tax = 10;
+        $tip = 20;
+        $result = restaurant_check($meal, $tax, $tip);
+        $this->assertEquals(130, $result);
+    }
+
+    public function testWithNoTip(){
+        $meal = 100;
+        $tax = 10;
+        $tip = 0;
+        $result = restaurant_check($meal, $tax, $tip);
+        $this->assertEquals(120, $result);
+    }
+
+}
+
+?>
